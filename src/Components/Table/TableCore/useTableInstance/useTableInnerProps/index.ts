0e60b6ce1bdaa780805de5f4f3caf50fda25b157
sunzhibin <sunzhibin@supcon.com>
2026-02-09 09:11:26 +0800

    [update] 优化Table

--- src/Components/Table/TableCore/useTableInstance/useTableInnerProps/index.ts
+++ src/Components/Table/TableCore/useTableInstance/useTableInnerProps/index.ts
@@ -19,10 +19,11 @@ const useTableInnerProps = <T, K = RowKeyType, S = any>(coreProps: TableCoreProp
 		borderWidth: coreProps.borderWidth ?? defaultBorderWidth,
 		theme: (coreProps.theme === 'dark' ? 'dark' : 'light') as TableCoreTheme,
 		data: useMemo(() => coreProps.data ?? [], [coreProps.data]),
-		// 修复问题，data数据变更但之更换了顺序，在显示范围内未变更的元素会导致父元素发生滚动，导致显示错位
+		// 修复问题，data数据变更但只更换了顺序，在显示范围内未变更的元素会导致父元素发生滚动，导致显示错位
 		// 根本问题为：data数据变更但之更换了顺序，在显示范围内未变更的元素的rowKey并未发生改变
 		// react渲染的优化机制渲染，跳过当前元素（或只对其属性进行修改），并移除或插入其他元素
-		// 浏览器渲染的优化机制，自动触发了body的滚动，对未发生改变的元素保持视口位置不变
+		// 浏览器滚动锚定行为，自动触发了body的滚动，对未发生改变的元素保持视口位置不变
+		// 再额外增加overflow-anchor: none;来取消浏览器滚动锚定行为
 		dataId: useMemo(() => new Date().valueOf(), [coreProps.data]),
 		rowKey: useMemo(() => coreProps.rowKey, [coreProps.rowKey?.toString()]),
 		highlightKeywords: useMemo(() => coreProps.highlightKeywords, [coreProps.highlightKeywords?.toString()]),
